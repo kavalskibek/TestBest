2f794be076703063a2ca00414f57592a880f0d53
orozobek-omurkulov <oroz1111omurkulov@gmail.com>
2025-05-29 17:03:13 +0600

    correct

--- src/test/java/org/example/framework/ApiBaseTest.java
+++ src/test/java/org/example/framework/ApiBaseTest.java
@@ -6,36 +6,35 @@ import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.BeforeSuite;
 
 public class ApiBaseTest {
-
     protected static String accessToken;
     protected static RequestSpecification spec;
 
-
-
     @BeforeSuite
     public void initApi() {
+        // 1) set the base URI
+        RestAssured.baseURI = ConfigLoader.get("amadeus.base_uri");
+        // 2) enable logging on failures
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 
-        //fetching token once
-        accessToken  = RestAssured
-                .given()
+        // 3) fetch the token
+        accessToken = RestAssured.given()
                 .contentType("application/x-www-form-urlencoded")
-                .formParam("grant_type", "client_credentials")
-                .formParam("client_id", ConfigLoader.get("amadeus.client_id"))
+                .formParam("grant_type",    "client_credentials")
+                .formParam("client_id",     ConfigLoader.get("amadeus.client_id"))
                 .formParam("client_secret", ConfigLoader.get("amadeus.client_secret"))
+                .log().all()
                 .when()
                 .post(ConfigLoader.get("amadeus.token_path"))
                 .then()
+                .log().all()
                 .statusCode(200)
                 .extract()
                 .jsonPath().getString("access_token");
 
-
-        //build a shared spec with auth header
-
+        // 4) build a shared RequestSpecification for later tests
         spec = new RequestSpecBuilder()
-                .setBaseUri(ConfigLoader.get("amadeus.base_uri"))
-                .addHeader("Authorization", "Bearer "+ accessToken)
+                .setBaseUri(RestAssured.baseURI)
+                .addHeader("Authorization", "Bearer " + accessToken)
                 .build();
-
     }
 }
--- src/test/java/org/example/tests/TokenTest.java
+++ src/test/java/org/example/tests/TokenTest.java
@@ -1,23 +1,17 @@
 package org.example.tests;
 
 import io.restassured.RestAssured;
-import static io.restassured.RestAssured.given;
-import io.restassured.response.Response;
+
 import org.example.framework.ApiBaseTest;
 import org.example.framework.ConfigLoader;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.given;
 
 public class TokenTest extends ApiBaseTest {
 
 
-    @BeforeClass
-    public void init() {
-        RestAssured.baseURI = ConfigLoader.get("amadeus.base_uri");
-    }
 
     @Test
     public  void fetchAmadeusToken() {
